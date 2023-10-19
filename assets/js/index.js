1434c8506ef9f40dbe00892fc14dd67cbf7039f3
Mithun Yadav <mithundesigner01@gmail.com>
2023-10-19 19:09:29 +0530

    it uploaded

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+
+// const rippleBtn = document.querySelector(".ripple-btn");
+
+// rippleBtn.addEventListener("mouseover",(e)=>{
+//   let valTop = rippleBtn.offsetTop;
+//   let valleft = rippleBtn.offsetLeft;
+//   let innerTopPosition = e.pageY-valTop;
+//   let innerLeftPosition = e.pageX-valleft;
+//   console.log(e.clientX);
+//   console.log(innerTopPosition,innerLeftPosition,valTop,valleft)
+//   rippleBtn.style.setProperty("--topPosition",`${innerTopPosition}px`);
+//    rippleBtn.style.setProperty("--leftPosition",`${innerLeftPosition}px`);
+// });
